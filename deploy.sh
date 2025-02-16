e6e9c8934202d7e640efb0259f9640be61cf59da
caocheng <caoc@live.com>
2025-02-16 17:07:58 +0800

    增加bd4vki站点的更新

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #/bin/sh
 
 #bd4two.nrlptt.com
+hostlist='nrlptt.com bh4tdv.nrlptt.com ba1gm.nrlptt.com bd4vki.nrlptt.com'
 
-hostlist='nrlptt.com bh4tdv.nrlptt.com ba1gm.nrlptt.com '
+time=`date "+%Y%m%d%H%M%S"`
 
 go build 
 
 for i in $hostlist ; do     
 echo "deploying to $i"
    scp udphub root@$i:
-   ssh root@$i "cd /nrllink; mv udphub udphub.bak ; cp /root/udphub . ; systemctl restart nrllink"
+   ssh root@$i "cd /nrllink; mv udphub udphub.$time ; cp /root/udphub . ; systemctl restart nrllink"
 done
